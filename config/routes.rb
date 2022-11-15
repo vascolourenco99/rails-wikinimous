10a8714d31085e8b72c3f951b1ec480d86d27952
vascocorreia <vascocorreia99@gmail.com>
2022-11-15 15:26:58 +0000

    exercise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit', as: 'article_edit'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
